040f8e9132660fb28ccbe55aa003cb98d10e9619
Ace <ace268868@gmail.com>
2024-08-21 19:37:33 +1200

    exercise 4

---
+++ exercise4.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    //create a string version of x
+const xAsString = x.toString();
+//start i at 0
+let i = 0;
+//check if x is === to reversed x
+for(j = xAsString.length -1 ;  j >= 0 ; j--){
+    if(xAsString.charAt(j) === xAsString.charAt(i)){
+     i++   
+    }else{
+        return false;
+    }
+}
+return true;
+};
--- test.js
+++ test.js
@@ -1,7 +1,18 @@
-let nums = [10,11,12,13]
-;
+const x = 10;
 
-nums.forEach((i) =>{
-    console.log(i)
-});
-
+function isPalindrome(){
+    //create a string version of x
+    const xAsString = x.toString();
+    //start i at 0
+    let i = 0;
+    //check if x is === to reversed x
+    for(j = xAsString.length -1 ;  j >= 0 ; j--){
+        if(xAsString.charAt(j) === xAsString.charAt(i)){
+         i++   
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
+isPalindrome()
