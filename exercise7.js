4dbcd4889f42f2853df15e8fb9594a6cfed2fc21
Ace <ace268868@gmail.com>
2024-08-30 08:48:42 +1200

    exercise7

--- exercise7.js
+++ exercise7.js
@@ -1,10 +1,6 @@
 function swap(inputArr, tuple) {
-    const val1 = inputArr.indexOf(tuple[0]);
-    inputArr[val1] = tuple[1]
-    const val2 = inputArr.indexOf(tuple[1], 2);
-    if(val2 !== -1){
-        inputArr[val2] = tuple[0]
-    }
-
+    const val1 = inputArr[tuple[0]]
+    inputArr[tuple[0]] = inputArr[tuple[1]]
+    inputArr[tuple[1]] = val1
     return inputArr
 }
